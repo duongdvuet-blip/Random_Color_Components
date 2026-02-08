f27ce61abf87d4406f7586d40165dfe1910874b5
duongdvuet-blip <duongdv.uet@gmail.com>
2026-02-08 21:02:44 +0700

    Add random color assignment for components

--- Random_Color_Components.py
+++ Random_Color_Components.py
@@ -2,79 +2,81 @@
 # Journal created by NADOSI on Sun Feb  8 20:53:52 2026 SE Asia Standard Time
 #
 import math
+import random
+import sys
 import NXOpen
 import NXOpen.Assemblies
-def main(args) : 
+def _collect_components(component):
+    components = []
+    if component is None:
+        return components
+    for child in component.GetChildren():
+        components.append(child)
+        components.extend(_collect_components(child))
+    return components
 
-    theSession  = NXOpen.Session.GetSession() #type: NXOpen.Session
-    workPart = theSession.Parts.Work
-    displayPart = theSession.Parts.Display
-    # ----------------------------------------------
-    #   Menu: Edit->Object Display...
-    # ----------------------------------------------
-    markId1 = theSession.SetUndoMark(NXOpen.Session.MarkVisibility.Invisible, "Start")
-    
-    theSession.SetUndoMarkName(markId1, "Object Color Dialog")
-    
-    # ----------------------------------------------
-    #   Dialog Begin Object Color
-    # ----------------------------------------------
-    markId2 = theSession.SetUndoMark(NXOpen.Session.MarkVisibility.Invisible, "Object Color")
-    
-    theSession.DeleteUndoMark(markId2, None)
-    
-    markId3 = theSession.SetUndoMark(NXOpen.Session.MarkVisibility.Invisible, "Object Color")
-    
-    theSession.DeleteUndoMark(markId3, None)
-    
-    theSession.SetUndoMarkName(markId1, "Object Color")
-    
-    theSession.DeleteUndoMark(markId1, None)
-    
-    markId4 = theSession.SetUndoMark(NXOpen.Session.MarkVisibility.Visible, "Edit Object Display")
-    
-    displayModification1 = theSession.DisplayManager.NewDisplayModification()
-    
-    displayModification1.ApplyToAllFaces = True
-    
-    displayModification1.ApplyToOwningParts = False
-    
-    displayModification1.NewColor = 186
-    
-    displayModification1.EndPointDisplayState = False
-    
-    objects1 = [NXOpen.DisplayableObject.Null] * 1 
-    component1 = workPart.ComponentAssembly.RootComponent.FindObject("COMPONENT SAN PHAM 1")
-    objects1[0] = component1
-    displayModification1.Apply(objects1)
-    
-    nErrs1 = theSession.UpdateManager.DoUpdate(markId4)
-    
-    displayModification1.Dispose()
-    markId5 = theSession.SetUndoMark(NXOpen.Session.MarkVisibility.Visible, "Edit Object Display")
-    
-    displayModification2 = theSession.DisplayManager.NewDisplayModification()
-    
-    displayModification2.ApplyToAllFaces = True
-    
-    displayModification2.ApplyToOwningParts = False
-    
-    displayModification2.EndPointDisplayState = False
-    
-    objects2 = [NXOpen.DisplayableObject.Null] * 1 
-    objects2[0] = component1
-    displayModification2.Apply(objects2)
-    
-    nErrs2 = theSession.UpdateManager.DoUpdate(markId5)
-    
-    theSession.DeleteUndoMark(markId5, "Edit Object Display")
-    
-    displayModification2.Dispose()
-    theSession.CleanUpFacetedFacesAndEdges()
-    
-    # ----------------------------------------------
-    #   Menu: Tools->Automation->Journal->Stop Recording
-    # ----------------------------------------------
-    
+
+def _component_from_object(obj, work_part):
+    if isinstance(obj, NXOpen.Assemblies.Component):
+        return obj
+    if hasattr(obj, "OwningComponent") and obj.OwningComponent is not None:
+        return obj.OwningComponent
+    if hasattr(obj, "OwningPart") and obj.OwningPart is not None:
+        root = work_part.ComponentAssembly.RootComponent
+        for component in _collect_components(root):
+            if component.Prototype == obj.OwningPart:
+                return component
+    return None
+
+
+def main(args):
+    the_session = NXOpen.Session.GetSession()
+    work_part = the_session.Parts.Work
+    ui = NXOpen.UI.GetUI()
+
+    selection_manager = ui.SelectionManager
+    selected_objects = selection_manager.GetSelectedObjects()
+
+    root_component = work_part.ComponentAssembly.RootComponent
+    if selected_objects:
+        components = []
+        for obj in selected_objects:
+            component = _component_from_object(obj, work_part)
+            if component is not None:
+                components.append(component)
+    else:
+        components = _collect_components(root_component)
+
+    if not components:
+        return
+
+    color_by_part = {}
+    random.seed()
+    for component in components:
+        prototype = component.Prototype
+        key = prototype.Tag if prototype is not None else component.Tag
+        if key not in color_by_part:
+            color_by_part[key] = random.randint(1, 216)
+
+    mark_id = the_session.SetUndoMark(
+        NXOpen.Session.MarkVisibility.Visible,
+        "Random Color Components",
+    )
+    display_modification = the_session.DisplayManager.NewDisplayModification()
+    display_modification.ApplyToAllFaces = True
+    display_modification.ApplyToOwningParts = False
+    display_modification.EndPointDisplayState = False
+
+    for component in components:
+        prototype = component.Prototype
+        key = prototype.Tag if prototype is not None else component.Tag
+        display_modification.NewColor = color_by_part[key]
+        display_modification.Apply([component])
+
+    the_session.UpdateManager.DoUpdate(mark_id)
+    display_modification.Dispose()
+    the_session.CleanUpFacetedFacesAndEdges()
+
+
 if __name__ == '__main__':
     main(sys.argv[1:])
