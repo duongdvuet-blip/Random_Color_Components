2cdd857ec0f238fd93df2e7ad40c380b1bb0c29e
duongdvuet-blip <duongdv.uet@gmail.com>
2026-02-08 20:55:01 +0700

    Add files via upload

--- Random_Color_Components.py
+++ Random_Color_Components.py
@@ -1,48 +1,80 @@
-# -*- coding: utf-8 -*-
+﻿# NX 2412
+# Journal created by NADOSI on Sun Feb  8 20:53:52 2026 SE Asia Standard Time
+#
+import math
 import NXOpen
 import NXOpen.Assemblies
+def main(args) : 
 
-def main():
-    session = NXOpen.Session.GetSession()
-    ui = NXOpen.UI.GetUI()
-    lw = session.ListingWindow
-    lw.Open()
-
-    sel = ui.SelectionManager
-    count = sel.GetNumSelectedObjects()
-
-    if count == 0:
-        ui.NXMessageBox.Show(
-            "Clear Component Color",
-            NXOpen.NXMessageBox.DialogType.Warning,
-            "No component selected"
-        )
-        return
-
-    disp_mod = session.DisplayManager.NewDisplayModification()
-
-    # ⭐ ĐÚNG TÊN THUỘC TÍNH TRONG NX 2206
-    disp_mod.RemoveOverrides = True
-    disp_mod.ApplyToAllFaces = True
-    disp_mod.ApplyToOwningParts = True
-
-    cleared = 0
-
-    for i in range(count):
-        obj = sel.GetSelectedObject(i)
-
-        if isinstance(obj, NXOpen.Assemblies.Component):
-            disp_mod.Apply([obj])
-            lw.WriteLine("Reset color override: " + obj.Name)
-            cleared += 1
-
-    disp_mod.Dispose()
-
-    ui.NXMessageBox.Show(
-        "Clear Component Color",
-        NXOpen.NXMessageBox.DialogType.Information,
-        "Reset color for {} component(s).".format(cleared)
-    )
-
-if __name__ == "__main__":
-    main()
+    theSession  = NXOpen.Session.GetSession() #type: NXOpen.Session
+    workPart = theSession.Parts.Work
+    displayPart = theSession.Parts.Display
+    # ----------------------------------------------
+    #   Menu: Edit->Object Display...
+    # ----------------------------------------------
+    markId1 = theSession.SetUndoMark(NXOpen.Session.MarkVisibility.Invisible, "Start")
+    
+    theSession.SetUndoMarkName(markId1, "Object Color Dialog")
+    
+    # ----------------------------------------------
+    #   Dialog Begin Object Color
+    # ----------------------------------------------
+    markId2 = theSession.SetUndoMark(NXOpen.Session.MarkVisibility.Invisible, "Object Color")
+    
+    theSession.DeleteUndoMark(markId2, None)
+    
+    markId3 = theSession.SetUndoMark(NXOpen.Session.MarkVisibility.Invisible, "Object Color")
+    
+    theSession.DeleteUndoMark(markId3, None)
+    
+    theSession.SetUndoMarkName(markId1, "Object Color")
+    
+    theSession.DeleteUndoMark(markId1, None)
+    
+    markId4 = theSession.SetUndoMark(NXOpen.Session.MarkVisibility.Visible, "Edit Object Display")
+    
+    displayModification1 = theSession.DisplayManager.NewDisplayModification()
+    
+    displayModification1.ApplyToAllFaces = True
+    
+    displayModification1.ApplyToOwningParts = False
+    
+    displayModification1.NewColor = 186
+    
+    displayModification1.EndPointDisplayState = False
+    
+    objects1 = [NXOpen.DisplayableObject.Null] * 1 
+    component1 = workPart.ComponentAssembly.RootComponent.FindObject("COMPONENT SAN PHAM 1")
+    objects1[0] = component1
+    displayModification1.Apply(objects1)
+    
+    nErrs1 = theSession.UpdateManager.DoUpdate(markId4)
+    
+    displayModification1.Dispose()
+    markId5 = theSession.SetUndoMark(NXOpen.Session.MarkVisibility.Visible, "Edit Object Display")
+    
+    displayModification2 = theSession.DisplayManager.NewDisplayModification()
+    
+    displayModification2.ApplyToAllFaces = True
+    
+    displayModification2.ApplyToOwningParts = False
+    
+    displayModification2.EndPointDisplayState = False
+    
+    objects2 = [NXOpen.DisplayableObject.Null] * 1 
+    objects2[0] = component1
+    displayModification2.Apply(objects2)
+    
+    nErrs2 = theSession.UpdateManager.DoUpdate(markId5)
+    
+    theSession.DeleteUndoMark(markId5, "Edit Object Display")
+    
+    displayModification2.Dispose()
+    theSession.CleanUpFacetedFacesAndEdges()
+    
+    # ----------------------------------------------
+    #   Menu: Tools->Automation->Journal->Stop Recording
+    # ----------------------------------------------
+    
+if __name__ == '__main__':
+    main(sys.argv[1:])
